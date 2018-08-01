9f6f034718f7fba72ced989b6f4f9bfd1f111a28
Guillaume GALLANI <guillaume.gallani@amadeus.com>
2018-08-01 16:13:19 -0400

    Connect to the DB

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
+import { MongolabService } from './mongolab.service';
 
 @Component({
   selector: 'app-root',
+  providers: [MongolabService],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  recLoc = 'RECLOC';
+  passengerDetails = {};
+  currentlyServedRank = '?';
+
+  constructor(mongoService: MongolabService) {
+    mongoService.getPassengerDetails(this.recLoc).subscribe(data => {
+        console.dir(data[0]);
+        this.passengerDetails = data[0];
+    });
+  }
 }
--- src/app/mongolab.service.ts
+++ src/app/mongolab.service.ts
@@ -1,8 +1,17 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable()
 export class MongolabService {
 
-  constructor() { }
+  baseURL = 'https://api.mlab.com/api/1/databases/virtual-token/collections/';
+  apiKey = 'm0lpQ0AtgHyiKNl1sIhYS7aYXdXDetVU';
 
+  constructor(private http: HttpClient) { }
+
+  getPassengerDetails(recLoc: string) {
+
+    var aURL = this.baseURL + 'Passengers?apiKey=' + this.apiKey;
+    return this.http.get(aURL);
+  }
 }
