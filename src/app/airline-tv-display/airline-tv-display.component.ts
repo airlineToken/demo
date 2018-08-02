583d47b20ae048ea448d1286beb23cebbdd3a838
Guillaume GALLANI <guillaume.gallani@amadeus.com>
2018-08-02 09:23:04 -0400

    Airline TV lists all queued pax

--- src/app/airline-tv-display/airline-tv-display.component.ts
+++ src/app/airline-tv-display/airline-tv-display.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { MongolabService } from '../mongolab.service';
+import { Observable} from 'rxjs/Rx';
 
 @Component({
   selector: 'app-airline-tv-display',
@@ -9,16 +10,43 @@ import { MongolabService } from '../mongolab.service';
 })
 export class AirlineTvDisplayComponent implements OnInit {
 
-  records = {};
+  queue = {};
+  passengers = {};
 
-  constructor(mongoService: MongolabService) {
-    mongoService.getRecordsinQueue().subscribe(data => {
-      console.dir(data[0]);
-      this.records = data;
+  constructor(private mongoService: MongolabService) {}
+
+  refreshQueueDetails() {
+    this.mongoService.getQueueDetails().subscribe(data => {
+      this.queue = data[0];
+    });
+  }
+
+  mapByRecloc(data: any[]) {
+    let result = {};
+    for (let i = 0; i < data.length; ++i) {
+      let pax = data[i];
+      result[pax.recLoc] = pax;
+    }
+
+    return result;
+  }
+
+  refreshPassengerDetails() {
+    this.mongoService.getPassengerDetails().subscribe(data => {
+        this.passengers = this.mapByRecloc(data);
     });
   }
 
   ngOnInit() {
+    this.refreshQueueDetails();
+    Observable.interval(5000).subscribe(data => {
+        this.refreshQueueDetails();
+    });
+
+    this.refreshPassengerDetails();
+    Observable.interval(5000).subscribe(data => {
+        this.refreshPassengerDetails();
+    });
   }
 
 }
